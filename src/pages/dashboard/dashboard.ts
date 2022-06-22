c90c0eacffa5f9efe04c4bf7162977057dcd404a
joocool <jcode7472@gmail.com>
2022-06-22 11:03:33 +0900

    FEAT : get ticker on dashboard

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,4 +1,24 @@
+import axios from 'axios';
 import {Options, Vue} from 'vue-class-component';
 
 @Options({})
-export default class Dashboard extends Vue {}
+export default class Dashboard extends Vue {
+
+    marketName : string = "";
+    tradePrice : string = "";
+
+    async mounted(): Promise<void>{
+        
+        const getTicker = () => {
+            axios.get(`https://api.upbit.com/v1/ticker?markets=KRW-BTC`)
+            .then((res) => {
+                this.marketName = res.data[0].market;
+                this.tradePrice = res.data[0].trade_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            });
+        }
+        getTicker();
+
+        setInterval(getTicker, 60000);
+    }
+
+}
